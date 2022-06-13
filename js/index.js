5f05b39acca115ab50a0cc5dd240311b49803960
Andrey Shalomitsky <akokha90@.mail.ru>
2022-06-13 12:51:14 +0500

    1

--- js/index.js
+++
@@ -1,8 +0,0 @@
-function smoothScrollingTest() {
-  const scroll = new LocomotiveScroll({
-    el: document.querySelector("[data-scroll-container]"),
-    smooth: true,
-    multiplier: 1,
-  });
-}
-smoothScrollingTest();
